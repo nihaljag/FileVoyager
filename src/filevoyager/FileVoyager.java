ce6becce046ec9ad295cb7e685ee523e9cf70264
Nihal J <72304446+nihaljag@users.noreply.github.com>
2021-12-22 06:46:12 +0530

    Add files via upload
    Added all project files

---
+++ src/filevoyager/FileVoyager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filevoyager;
+
+/**
+ *
+ * @author Nihal J
+ */
+public class FileVoyager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Welcome().setVisible(true);
+    }
+    
+}
